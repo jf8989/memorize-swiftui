8331241a4f653ea2e48c48c31f423bbf52943b5f
Juan Francisco Marcenaro Arellano <juanframarcenaro@ravn.co>
2025-07-17 12:56:11 -0500

    - added main title - added my own comments - changed button colors

--- Memorize/Memorize/MemorizeGameView.swift
+++ Memorize/Memorize/MemorizeGameView.swift
@@ -2,18 +2,41 @@ import SwiftUI
 
 struct MemorizeGameView: View {
     
+    // This is my hardcoded array of emojis.  The idea later will be to have at least 4 sets of these that get picked randomly.
     let emojis = ["🦄", "🐇", "🐰", "🐹", "🐻", "🐼", "🐨", "🐿️", "🐇", "🦄", "🐰", "🐹", "🐻", "🐼", "🐨", "🐿️"]
     
+    // We add state so that it's modifyiable from anywhere in this file.
     @State var cardCount: Int = 4
     
     var body: some View {
-        VStack {
-            cards
-            cardCountAdjusters
+        VStack {  // we're aligning all of our views vertically in order to organize the UI
+            title
+            cards // we place the card's grid at the top
+            Spacer()  // we add a space to push them away from each other all the way to the edges
+            cardCountAdjusters  // we insert the button's view here, defaulting all the way to the bottom of the screen
         }
         .padding()
     }
     
+    let gameTitle: String = "Memorize"
+    var title: some View {
+        Text(gameTitle)
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .fontDesign(.rounded)
+            .foregroundColor(.teal)
+    }
+    
+    // main grid where the cards reside
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
+        }
+    }
+    
+    // horizontal stack for the buttons
     var cardCountAdjusters: some View {
         HStack {
             cardRemover
@@ -24,35 +47,29 @@ struct MemorizeGameView: View {
         .font(.title)
     }
     
-    var cards: some View {
-        HStack {
-            ForEach(0..<cardCount, id: \.self) { index in
-                CardView(content: emojis[index])
-            }
-        }
+    // Dynamically creating the buttons and their logic
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+                cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
     }
     
-    var cardRemover: some View {
-        Button(action: {
-            if cardCount > 1  {
-                cardCount -= 1
-            }
-        }, label: {
-            Image(systemName: "rectangle.stack.fill.badge.minus")
-        })
+    // we're calling the function with different parameters depending on the desired action
+    var cardRemover: some View {  // removes a card
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+            .foregroundColor(.teal)
     }
     
-    var cardAdder: some View {
-        Button(action: {
-            if cardCount < emojis.count {
-                cardCount += 1
-            }
-        }, label: {
-            Image(systemName: "rectangle.stack.fill.badge.plus")
-        })
+    var cardAdder: some View {  // adds a card
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.fill.badge.plus")
+            .foregroundColor(.teal)
     }
 }
 
+// basic card creation using a separate struct
 struct CardView: View {
     let content: String
     @State var isFaceUp = false
@@ -75,24 +92,7 @@ struct CardView: View {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Notes:  I'm confused as to why we need to create a different struct CardView instead of simply creating a view within the main struct, MemorizeGameView.  Is it that, in order to create a visual element, we must use a struct?  One view per struct?
 
 #Preview {
     MemorizeGameView()
