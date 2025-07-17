f86cc1350a626d3b12fe8be75201f6e8d9e96269
Juan Francisco Marcenaro Arellano <juanframarcenaro@ravn.co>
2025-07-17 08:19:48 -0500

    improving UI control

--- Memorize/Memorize/MemorizeGameView.swift
+++ Memorize/Memorize/MemorizeGameView.swift
@@ -1,26 +1,36 @@
-//
-//  ContentView.swift
-//  Memorize
-//
-//  Created by Juan Francisco Marcenaro Arellano on 16/07/25.
-//
-
 import SwiftUI
 
 struct MemorizeGameView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .foregroundColor(Color.orange)
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("It's me, Mario!")
+    var body: some View {  // "some" is there to guarantee that a "view", regardless of the type, is returned.
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        
         .padding()
     }
 }
 
-
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {  // this is a "computed property".
+            // We declare a new variable that holds the base form that we're going to use all over the place instead of diplicating code.  It seems we have to specify the same shape or form every time we want to modify any of its parameters, which makes it using a base variable efficient.
+            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.foregroundColor(.white).shadow(radius: 2.5)
+                base.strokeBorder(lineWidth: 2).foregroundColor(.orange)
+                Text("🙈").font(.largeTitle)
+            } else {
+                base.foregroundColor(.yellow).shadow(radius: 2.5)
+                Text("?").bold().font(.largeTitle).fontDesign(.serif)
+            }
+        }
+    }
+}
 
 
 
